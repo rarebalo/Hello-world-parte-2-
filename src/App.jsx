db513b002027ed104809d8452faa9bd8331da019
Roberto <roberto.v.arebalo@gmail.com>
2024-01-03 21:50:23 -0300

    agrego el saludo de mi amgo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
-import'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Amigo from "./components/TituloSecundario";
 
-function App() { 
-  return <h1 className='text-center display-3'>Hello world</h1>;
+function App() {
+  return (
+    <>
+      <h1 className="text-center display-3">Hello world</h1>;
+      <Amigo></Amigo>
+    </>
+  );
 }
 export default App;
---
+++ src/components/TituloSecundario.jsx
@@ -0,0 +1,5 @@
+const Amigo = () =>{
+    return <h1>Hello my friend!</h1>
+}
+
+export default Amigo;
