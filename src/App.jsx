a0fa245b3685ae569045e0ac889f54e2096291d2
Roberto <roberto.v.arebalo@gmail.com>
2024-01-03 21:39:12 -0300

    le doy estilo al h1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-function App() {
-  return (<h1>Hello world</h1>)
+import'bootstrap/dist/css/bootstrap.min.css';
+
+function App() { 
+  return <h1 className='text-center display-3'>Hello world</h1>;
 }
 export default App;
